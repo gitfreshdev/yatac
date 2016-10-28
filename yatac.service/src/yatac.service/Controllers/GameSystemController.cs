dea3580461f57ee3634fb1ecb7ab875ee37f2a74
willy1977 <leemw1977@outlook.com>
2016-10-28 21:33:51 +0100

    Added test project, added first test and started GameSystemController

---
+++ yatac.service/src/yatac.service/Controllers/GameSystemController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace yatac.service.Controllers
+{
+    public class GameSystemController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            //return View();
+            return null;
+        }
+
+        public IActionResult GetSystemById(System.Guid id)
+        {
+            if (id.Equals(Guid.Empty))
+            {
+                throw new ArgumentOutOfRangeException(nameof(id), id.ToString(), "id cannot be an empty guid.");
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ yatac.service/test/Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+using yatac.service.Controllers;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void GetSystemById_EmptyGuidArgumentException() 
+        {
+            GameSystemController gameSystemController = new GameSystemController();
+            Assert.Throws<ArgumentOutOfRangeException>(() => gameSystemController.GetSystemById(Guid.Empty));
+        }
+    }
+}
